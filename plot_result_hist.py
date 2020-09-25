acf23422238e9f21724f52a8f028386f6ef60159
U-NDC\gmosby <gmosby@gs66-bologne.ndc.nasa.gov>
2020-09-25 16:30:17 -0400

    Added commandline conditional statement to plotting script.

--- plot_result_hist.py
+++ plot_result_hist.py
@@ -3,22 +3,24 @@
 
 import re
 import pandas as pd
+import sys
 import numpy as np
 # %matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn as sb
 
 
-
+#Add pyplot histogram function
 
 def pairwisescat(cfit):
+    """Plots pairwise histograms and scatterplots using seaborn."""
     
     df = pd.read_table(cfit, header=None)
-    df.index = ['Extinction (A_v)', 'Mass Frac Y', 'Mass Frac I1', 'Mass Frac I2', 'Mass Frac O']
+    df.index = ['Fitness Metric', 'Mass Frac Y', 'Mass Frac I1', 'Mass Frac I2', 'Mass Frac O']
     df = df.T
     sb.set_style("darkgrid")
 
-    pairplots = sb.PairGrid(df,  hue='Extinction (A_v)',palette='husl')#, hue='Extinction (A_v)', height = 3, aspect = 3, layout_pad=2)
+    pairplots = sb.PairGrid(df)#,  hue='Extinction (A_v)',palette='husl')#, hue='Extinction (A_v)', height = 3, aspect = 3, layout_pad=2)
     plt.gcf().subplots_adjust(wspace=0, hspace=0)
     pairplots.map_diag(sb.histplot)
     pairplots.map_offdiag(sb.scatterplot)
@@ -26,4 +28,18 @@ def pairwisescat(cfit):
     plt.show()
 
 #     plt.tight_layout()
-pairwisescat('tau_2gyr_newdfk_sn_020_ps_000_r000_model_fits.cfit')
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 3:
+        print("Incorrect number of arguments. Please provide a .cfit file name and plot type (pair or none).")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    plot_type = sys.argv[2]
+    
+    if plot_type == 'pair':
+        pairwisescat(filename)
+    else:
+        pass
+        #func(filename)
