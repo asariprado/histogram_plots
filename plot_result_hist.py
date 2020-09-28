1022594e312610b7bfd30adbfaa8591f8ea3d939
asariprado <62832051+asariprado@users.noreply.github.com>
2020-09-28 00:34:36 -0400

    Added pyplot histogram plotting function

--- plot_result_hist.py
+++ plot_result_hist.py
@@ -10,9 +10,40 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 
-#Add pyplot histogram function
+def plot_hist(cfit):
+        
+    """Plots histograms for a pandas DataFrame using pyplot."""
+
+    df = pd.read_table(cfit, header=None) 
+    df.index = ['Fitness Metric', 'Mass Frac Y', 'Mass Frac I1', 'Mass Frac I2', 'Mass Frac O']
+    
+    axs = plt.figure(figsize=(6*2.5,5*2)).subplots(2, 3, sharey=True)
+    axs = trim_axs(axs,len(df.index))
+        
+    for i in range(len(df.index)):
+        axs.flat[i].hist(df.iloc[i],edgecolor='white', linewidth=1)
+        axs.flat[i].set_title(df.index[i],weight='bold', size=14)
+        axs[i].tick_params(axis='x', labelrotation=30)
+        plt.xticks(ha='right')
+        axs.flat[i].ticklabel_format(scilimits=(-3,0))
+    
+    plt.show()
+        
+
+def trim_axs(axs, N):
+        """
+        Reduce *axs* to *N* Axes. All further Axes are removed from the figure. 
+        (removes an axis so that way there wont be a plot at that entry..blank space)
+        """
+        axs = axs.flat
+        for ax in axs[N:]:
+            ax.remove()
+
+        return axs[:N]
+
 
 def pairwisescat(cfit):
+    
     """Plots pairwise histograms and scatterplots using seaborn."""
     
     df = pd.read_table(cfit, header=None)
@@ -20,14 +51,15 @@ def pairwisescat(cfit):
     df = df.T
     sb.set_style("darkgrid")
 
-    pairplots = sb.PairGrid(df)#,  hue='Extinction (A_v)',palette='husl')#, hue='Extinction (A_v)', height = 3, aspect = 3, layout_pad=2)
-    plt.gcf().subplots_adjust(wspace=0, hspace=0)
+    pairplots = sb.PairGrid(df)
+#     plt.gcf().subplots_adjust(wspace=0, hspace=0)
+#     pairplots.fig.set_size_inches(8,8)
     pairplots.map_diag(sb.histplot)
     pairplots.map_offdiag(sb.scatterplot)
     pairplots.add_legend()
     plt.show()
 
-#     plt.tight_layout()
+
 
 if __name__ == "__main__":
     
@@ -39,7 +71,10 @@ if __name__ == "__main__":
     plot_type = sys.argv[2]
     
     if plot_type == 'pair':
+        plot_hist(filename)
         pairwisescat(filename)
     else:
-        pass
-        #func(filename)
+        # else if not 'pair', just plot the histograms
+        plot_hist(filename)
+        
+
